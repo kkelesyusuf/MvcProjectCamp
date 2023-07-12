3f53476af7b84f8f0bc1d4490843dbb72a05b916
kkelesyusuf <kkelesyusuf@gmail.com>
2023-07-12 04:36:57 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repository = new GenericRepository<Category>();
+        public List<Category> TGetAll()
+        {
+            return repository.List();
+        }
+        public void TAddCategory(Category p)
+        {
+            if(p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryDescription == "" || p.CategoryName.Length > 51)
+            {
+                //hata mesajı
+            }
+            else
+            {
+                repository.Add(p);
+            }
+        }
+    }
+}
